086918a6ed2172b5dbc4e1a14deb2e0536fee240
Adrien CHABAUD <59933787+Adrien-CHABAUD@users.noreply.github.com>
2024-04-05 13:04:46 +0100

    Initial Commit

---
+++ YolkTimer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  YolkTimer
+//
+//  Created by Adrien CHABAUD on 2024-04-05.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ YolkTimer/YolkTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  YolkTimerApp.swift
+//  YolkTimer
+//
+//  Created by Adrien CHABAUD on 2024-04-05.
+//
+
+import SwiftUI
+
+@main
+struct YolkTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
