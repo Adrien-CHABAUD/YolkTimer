cca80416b503f8c11c438106adb0a7f0dfa91975
Adrien CHABAUD <59933787+Adrien-CHABAUD@users.noreply.github.com>
2024-04-05 15:20:00 +0100

    Background

---
+++ YolkTimer/StartView/StartView.swift
@@ -0,0 +1,18 @@
+//
+//  StartView.swift
+//  YolkTimer
+//
+//  Created by Adrien CHABAUD on 2024-04-05.
+//
+
+import SwiftUI
+
+struct StartView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StartView()
+}
---
+++ YolkTimer/StartView/StartViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  StartViewModel.swift
+//  YolkTimer
+//
+//  Created by Adrien CHABAUD on 2024-04-05.
+//
+
+import Foundation
