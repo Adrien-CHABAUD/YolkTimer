c5257a261051dec9cdfccb5dbf420b512dcbb9c0
Adrien CHABAUD <59933787+Adrien-CHABAUD@users.noreply.github.com>
2024-05-30 19:38:50 +0100

    Create "test_TimerCountdown" unit test.

--- YolkTimerTests/YolkTimerTests.swift
+++ YolkTimerTests/YolkTimerTests.swift
@@ -6,6 +6,7 @@
 //
 
 import XCTest
+import Combine
 @testable import YolkTimer
 
 final class YolkTimerTests: XCTestCase {
@@ -14,6 +15,7 @@ final class YolkTimerTests: XCTestCase {
 //
 //    }
     var startViewModel: StartViewModel!
+    var cancellables: Set<AnyCancellable> = []
     
     override func setUp() {
         super.setUp()
@@ -25,6 +27,7 @@ final class YolkTimerTests: XCTestCase {
     override func tearDown() {
         // Clean up after each test
         startViewModel = nil
+        cancellables = []
         super.tearDown()
     }
     
@@ -63,5 +66,50 @@ final class YolkTimerTests: XCTestCase {
         // Then
         XCTAssertEqual(Int(startViewModel.remainingTime), expectedHardEggTime, "Hard egg timer should be set to 9 minutes.")
     }
+    
+    func test_TimerCountdown() throws {
+        // Given
+        startViewModel.pickerSelection = .runnyState // 3 minutes in seconds
+        startViewModel.selectTime()
+        
+        // When
+        startViewModel.isRunning = true
+        print("Timer started with remaining time: \(startViewModel.remainingTime)")
+        
+        // Set up expectation
+        let expectation = self.expectation(description: "Timer should countdown")
+        
+        // Subscribe to changes in remainingTime
+        startViewModel.$remainingTime
+            .dropFirst() // Ignore the initial value
+            .sink { remainingTime in
+                print("Remaining time: \(remainingTime)")
+                if remainingTime < 180 {
+                    expectation.fulfill()
+                }
+            }
+            .store(in: &cancellables)
+        
+        // Then
+        waitForExpectations(timeout: 2, handler: nil)
+        XCTAssertLessThan(startViewModel.remainingTime, 180, "Timer should count down.")
+    }
+    
+    // Timer func
+    // test setting timer (3 cases) X
+    // Test timer countdown X
+    // Test timer completion
+    
+    // Notif func
+    // test notification is sent when timer completes
+    
+    // UI Elements
+    // Test UI components are present and visible
+    // Test UX: tapping on button changes the egg cooking type
+    
+    // Edge test
+    // Test rapid timer changes does not cause errors or incorrect timer durations
+    // Verify that app handles interruptions (incoming calls) without loosing timer progress
+    
 
 }
